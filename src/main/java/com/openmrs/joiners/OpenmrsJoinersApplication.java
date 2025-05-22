8786caa4b343516fc2a9436c72ace40e1b29a532
Christopher Miiro <christopher.miiro@gmail.com>
2025-05-22 08:52:38 +0300

    Rename package and remove defaultspring boot test

--- src/main/java/com/openmrs/joiners/OpenmrsJoinersApplication.java
+++ src/main/java/com/openmrs/joiners/OpenmrsJoinersApplication.java
@@ -1,4 +1,4 @@
-package com.newopenmrs.joinners;
+package com.openmrs.joiners;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
